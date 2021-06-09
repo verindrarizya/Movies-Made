1d03c6fbe423d4107f77db540568f333e5f4a52f
verindrarizya <v.rizya22@gmail.com>
2021-06-10 01:49:27 +0700

    adding leakcanary, obfuscation, encryption, and certificate pinning

--- core/src/main/java/com/verindrarizya/core/di/CoreModule.kt
+++ core/src/main/java/com/verindrarizya/core/di/CoreModule.kt
@@ -9,6 +9,9 @@ import com.verindrarizya.core.data.source.remote.network.ApiAssets.BASE_URL
 import com.verindrarizya.core.data.source.remote.network.ApiService
 import com.verindrarizya.core.domain.repository.IMovieRepository
 import com.verindrarizya.core.utils.AppExecutors
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
@@ -24,8 +27,11 @@ val databaseModule = module {
     }
 
     single {
+        val passphrase: ByteArray = SQLiteDatabase.getBytes("VerindraMovies".toCharArray())
+        val factory = SupportFactory(passphrase)
         Room.databaseBuilder(androidContext(), MovieDatabase::class.java, "movie.db")
             .fallbackToDestructiveMigration()
+            .openHelperFactory(factory)
             .build()
     }
 }
@@ -33,10 +39,17 @@ val databaseModule = module {
 val networkModule = module {
 
     single {
+        val hostname = "developers.themoviedb.org"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/kBf1bI1z28Ytbj7xOYuejUOmxHRpY24T7W8mscSQ+tI=")
+            .add(hostname, "sha256/jQJTbIh0grw0/1TkHSumWb+Fs0Ggogr621gT3PvPKG0=")
+            .add(hostname, "sha256/C5+lpZ7tcVwmwQIMcRtPbsQtWLABXhQzejna0wHFr8M=")
+            .build()
         OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
 
